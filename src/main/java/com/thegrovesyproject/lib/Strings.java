a61cfdf1a6ec7840b40c232ddba56ff005974a73
e <y1999412@163.com>
2018-10-22 20:56:48 +0800

    清理文件（10）

--- src/main/java/com/thegrovesyproject/lib/Strings.java
+++
@@ -1,9 +0,0 @@
-package com.thegrovesyproject.lib;
-
-public class Strings {
-
-	public static final String MODID = "tm";
-	public static final  String name = "Tutorial mod";
-	public static final String version = "0.01 ALPHA";
-	
-}
