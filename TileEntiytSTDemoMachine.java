8dc41392a540e79d2b5a655f5ae6512fad706c04
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2018-10-22 18:35:14 +0800

    Add files via upload
    按照教程添加了风车用于发电但尚未完成。

---
+++ TileEntiytSTDemoMachine.java
@@ -0,0 +1,8 @@
+package com.github.mrstop.stdemo.tileentity;
+
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.tileentity.TileEntity;
+
+public abstract class TileEntiytSTDemoMachine extends TileEntity implements ISidedInventory {
+
+}
