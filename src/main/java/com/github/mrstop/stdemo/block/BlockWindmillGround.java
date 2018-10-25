838cc58e09ad97a4b3ee35a9fdc09dd05f17333e
e <y1999412@163.com>
2018-10-25 13:57:36 +0800

    Revert "修改风车选中时的高亮"
    This reverts commit 3634a0816fd250d7436c402cddb998ccccebfdd8.

--- src/main/java/com/github/mrstop/stdemo/block/BlockWindmillGround.java
+++ src/main/java/com/github/mrstop/stdemo/block/BlockWindmillGround.java
@@ -6,7 +6,6 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
@@ -116,11 +115,6 @@ public class BlockWindmillGround extends Block {
             default:
                 this.setBlockBounds(0, 0, 0, 1, 0.3F, 1);
         }
-    }
-
-    @Override
-    public AxisAlignedBB getCollisionBoundingBoxFromPool(World worldIn, int x, int y, int z) {
-        return super.getCollisionBoundingBoxFromPool(worldIn, x, y, z);
     }
 
     @Override
