3459742dabc432b48209d5a86f230248b3790732
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-04-03 14:07:30 +0800

    添加Random对象

--- src/main/java/com/github/mrstop/stdemo/STDemo.java
+++ src/main/java/com/github/mrstop/stdemo/STDemo.java
@@ -11,6 +11,8 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 
+import java.util.Random;
+
 
 /**
  * @author 闫坤炜
@@ -23,6 +25,7 @@ public class STDemo {
     public static final String MOD_DOMAIN = "stdemo:";
     public static final String NAME = "STDemo";
     public static final String VERSION = "1.0.0";
+    public static final Random random = new Random();
     public static final float PI = 3.1415923F;
     public static final int GUIIDMetalFurnace = 1;
     public static final int GUIIDDemo = 2;
