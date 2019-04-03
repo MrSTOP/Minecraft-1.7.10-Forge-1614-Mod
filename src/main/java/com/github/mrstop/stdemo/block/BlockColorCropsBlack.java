1f3b3778d379f3097bd4c6a6becc411678db0703
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-04-03 19:25:09 +0800

    为黑色Color方块添加getItemDropped方法并注册方块材质

--- src/main/java/com/github/mrstop/stdemo/block/BlockColorCropsBlack.java
+++ src/main/java/com/github/mrstop/stdemo/block/BlockColorCropsBlack.java
@@ -1,8 +1,15 @@
 package com.github.mrstop.stdemo.block;
 
+import com.github.mrstop.stdemo.STDemo;
 import com.github.mrstop.stdemo.core.util.STDemoHelper;
+import com.github.mrstop.stdemo.item.ItemLoader;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.Item;
 import net.minecraft.util.IIcon;
+
+import java.util.Random;
 
 public class BlockColorCropsBlack extends BlockColorCrops {
 
@@ -13,7 +20,18 @@ public class BlockColorCropsBlack extends BlockColorCrops {
     }
 
     @Override
+    public Item getItemDropped(int meta, Random rand, int fortune) {
+        if (meta != maxGrowthStage) {
+            return Item.getItemFromBlock(this);
+        } else {
+            return ItemLoader.seedsColorBlack;
+        }
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
     public void registerIcons(IIconRegister iconRegister) {
-        STDemoHelper.registerIconArray(iconRegister, icons, "color_black_");
+        STDemoHelper.registerIconArray(iconRegister, icons, "color_crop_", 0, icons.length - 1);
+        icons[icons.length - 1] = iconRegister.registerIcon(STDemo.MOD_DOMAIN + "color_black");
     }
 }
