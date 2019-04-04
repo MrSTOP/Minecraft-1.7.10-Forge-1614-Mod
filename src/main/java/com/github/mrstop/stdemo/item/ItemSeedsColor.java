c01d351cfecbf218700e7f59e7363de2e780135f
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-04-04 13:09:37 +0800

    将ItemSeedsColor(彩色种子类)调整为虚基类，并添加构造方法

--- src/main/java/com/github/mrstop/stdemo/item/ItemSeedsColor.java
+++ src/main/java/com/github/mrstop/stdemo/item/ItemSeedsColor.java
@@ -1,24 +1,32 @@
 package com.github.mrstop.stdemo.item;
 
-import com.github.mrstop.stdemo.block.BlockLoader;
 import com.github.mrstop.stdemo.creativetab.CreativeTabsLoader;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemSeeds;
 
-public class ItemSeedsColor extends ItemSeeds {
+public abstract class ItemSeedsColor extends ItemSeeds {
 
-        public ItemSeedsColor() {
-            super(BlockLoader.colorBlock, Blocks.farmland);
-            this.setUnlocalizedName("seedsColor");
-            this.setCreativeTab(CreativeTabsLoader.tabSTDemo);
-        }
+    public ItemSeedsColor(Block plant, String unlocalizedName) {
+        this(plant, Blocks.farmland, unlocalizedName);
+    }
+
+    public ItemSeedsColor(Block plant, Block onBlock, String unlocalizedName) {
+        this(plant, onBlock, unlocalizedName, CreativeTabsLoader.tabSTDemo);
+    }
+
+    public ItemSeedsColor(Block plant, Block onBlock, String unlocalizedName, CreativeTabs creativeTab) {
+        super(plant, onBlock);
+        this.setUnlocalizedName(unlocalizedName);
+        this.setCreativeTab(creativeTab);
+    }
 
     @Override
     @SideOnly(Side.CLIENT)
-    public void registerIcons(IIconRegister iIconRegister) {
-        this.itemIcon = iIconRegister.registerIcon("stdemo:seeds_color");
-    }
+    abstract public void registerIcons(IIconRegister iIconRegister);
+//        this.itemIcon = iIconRegister.registerIcon("stdemo:seeds_color");
 }
