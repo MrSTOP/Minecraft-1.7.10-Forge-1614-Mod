1efe78a048446cc2c7d66c72fb834d6884bab8c2
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-04-04 14:29:03 +0800

    添加所有从BlockBush中继承的方法

--- src/main/java/com/github/mrstop/stdemo/block/BlockColorCrops.java
+++ src/main/java/com/github/mrstop/stdemo/block/BlockColorCrops.java
@@ -13,10 +13,13 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.EnumPlantType;
 import net.minecraftforge.oredict.OreDictionary;
 
 import java.util.ArrayList;
@@ -26,8 +29,7 @@ public class BlockColorCrops extends BlockCrops {
 
     protected static final int maxGrowthStage = 7;
 
-//    @SideOnly(Side.CLIENT)
-    protected IIcon[] icons = new IIcon[8];
+    protected IIcon[] icons = new IIcon[maxGrowthStage + 1];
 
     public BlockColorCrops() {
         this.setTickRandomly(true);
@@ -40,10 +42,65 @@ public class BlockColorCrops extends BlockCrops {
         this.disableStats();
     }
 
+    ////BlockCrops
+    @Override
+    public boolean canPlaceBlockAt(World worldIn, int x, int y, int z) {
+        return super.canPlaceBlockAt(worldIn, x, y, z);
+    }
+
     @Override
     protected boolean canPlaceBlockOn(Block ground) {
         return ground == Blocks.farmland;
     }
+
+    @Override
+    public void onNeighborBlockChange(World worldIn, int x, int y, int z, Block neighbor) {
+        super.onNeighborBlockChange(worldIn, x, y, z, neighbor);
+    }
+
+    @Override
+    public void updateTick(World world, int x, int y, int z, Random rand) {
+        super.updateTick(world, x, y, z, rand);
+        int growStage = world.getBlockMetadata(x, y, z) + 1;
+        if (growStage > maxGrowthStage) {
+            growStage = maxGrowthStage;
+        }
+        world.setBlockMetadataWithNotify(x, y, z, growStage, 2);
+
+    }
+
+    //检测方块是否能继续存在，不能则以物品形式掉落
+    @Override
+    protected void checkAndDropBlock(World worldIn, int x, int y, int z) {
+        super.checkAndDropBlock(worldIn, x, y, z);
+    }
+
+    @Override
+    public boolean canBlockStay(World worldIn, int x, int y, int z) {
+        return super.canBlockStay(worldIn, x, y, z);
+    }
+
+    @Override
+    public AxisAlignedBB getCollisionBoundingBoxFromPool(World worldIn, int x, int y, int z) {
+        return super.getCollisionBoundingBoxFromPool(worldIn, x, y, z);
+    }
+
+    @Override
+    public boolean isOpaqueCube() {
+//        return true;
+        return super.isOpaqueCube();
+    }
+
+    @Override
+    public boolean renderAsNormalBlock() {
+        return super.renderAsNormalBlock();
+    }
+
+    @Override
+    public int getRenderType() {
+        return 1;
+    }
+    ////BlockCrops
 
     public void incrementGrowStage(World world, Random rand, int X, int Y, int Z) {
         int growStage = world.getBlockMetadata(X, Y, Z) + MathHelper.getRandomIntegerInRange(rand, 2, 5);
@@ -59,63 +116,56 @@ public class BlockColorCrops extends BlockCrops {
             return true;
         }
         else {
-            for (int i : OreDictionary.getOreIDs(player.getHeldItem())) {
-                if (OreDictionary.getOreID("dyeBlack") == i) {
-                    if (Math.random() <= 0.2) {
+            if (Math.random() <= 0.2) {
+                for (int i : OreDictionary.getOreIDs(player.getHeldItem())) {
+                    if (OreDictionary.getOreID("dyeBlack") == i) {
                         player.addChatMessage(new ChatComponentText("Black!"));
                         worldIn.setBlock(x, y, z, BlockLoader.colorBlockBlack);
                     }
+                }
+            }
+            for (String str : OreDictionary.getOreNames()) {
+                if (str.contains("dye")) {
+                    player.addChatMessage(new ChatComponentText(str));
                 }
             }
             return true;
         }
     }
 
+
+
     @Override
     public Item getItemDropped(int meta, Random rand, int fortune) {
-        if (meta != 7) {
-            return Item.getItemFromBlock(this);
-        } else {
-            return ItemLoader.color;
-        }
+        return meta == maxGrowthStage ? this.getCrop() : getSeed();
     }
 
     @Override
-    public int getRenderType() {
-        return 1;
+    public int quantityDropped(Random random) {
+        return super.quantityDropped(random);
     }
+
 
     @Override
     @SideOnly(Side.CLIENT)
     public IIcon getIcon(int side, int growState) {
+        if (growState < 0 || growState > maxGrowthStage) {
+            return icons[0];
+        }
         return icons[growState];
     }
 
     @Override
-    public boolean canFertilize(World worldIn, int x, int y, int z, boolean isClient) {
-        return worldIn.getBlockMetadata(x, y, z) != maxGrowthStage;
+    public void dropBlockAsItemWithChance(World worldIn, int x, int y, int z, int meta, float chance, int fortune) {
+        super.dropBlockAsItemWithChance(worldIn, x, y, z, meta, chance, fortune);
     }
 
     @Override
-    public boolean shouldFertilize(World worldIn, Random random, int x, int y, int z) {
-        return true;
+    @SideOnly(Side.CLIENT)
+    public Item getItem(World worldIn, int x, int y, int z) {
+        return super.getItem(worldIn, x, y, z);
     }
 
-    @Override
-    public void  fertilize(World worldIn, Random random, int x, int y, int z) {
-        incrementGrowStage(worldIn, random, x, y, z);
-    }
-
-    @Override
-    public void updateTick(World world, int x, int y, int z, Random rand) {
-        super.updateTick(world, x, y, z, rand);
-        int growStage = world.getBlockMetadata(x, y, z) + 1;
-        if (growStage > maxGrowthStage) {
-            growStage = maxGrowthStage;
-        }
-        world.setBlockMetadataWithNotify(x, y, z, growStage, 2);
-
-    }
 
     @Override
     protected Item getSeed() {
@@ -129,16 +179,51 @@ public class BlockColorCrops extends BlockCrops {
 
     @Override
     public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) {
-        ArrayList<ItemStack> ret = super.getDrops(world, x, y, z, metadata, fortune);
+        ArrayList<ItemStack> drops = super.getDrops(world, x, y, z, metadata, fortune);
         if (metadata >= 7) {
             for (int i = 0; i < 3 + fortune; ++i) {
                 if (world.rand.nextInt(15) <= metadata) {
-                    ret.add(new ItemStack(this.getCrop(), 1, 0));
+                    drops.add(new ItemStack(this.getCrop(), 1, 0));
                 }
             }
         }
-        return ret;
+        return drops;
     }
+
+    ////IGrowable
+    @Override
+    public boolean canFertilize(World worldIn, int x, int y, int z, boolean isClient) {
+        return worldIn.getBlockMetadata(x, y, z) != maxGrowthStage;
+    }
+
+    @Override
+    public boolean shouldFertilize(World worldIn, Random random, int x, int y, int z) {
+        return true;
+    }
+
+    @Override
+    public void fertilize(World worldIn, Random random, int x, int y, int z) {
+        incrementGrowStage(worldIn, random, x, y, z);
+    }
+    ////IGrowable
+
+    ////IPlantable
+    //作物类型（可以种植的环境）
+    @Override
+    public EnumPlantType getPlantType(IBlockAccess world, int x, int y, int z) {
+        return EnumPlantType.Plains;
+    }
+
+    @Override
+    public Block getPlant(IBlockAccess world, int x, int y, int z) {
+        return this;
+    }
+
+    @Override
+    public int getPlantMetadata(IBlockAccess world, int x, int y, int z) {
+        return world.getBlockMetadata(x, y, z);
+    }
+    ////IPlantable
 
     @Override
     @SideOnly(Side.CLIENT)
