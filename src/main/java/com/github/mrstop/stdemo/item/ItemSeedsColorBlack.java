5d3f0fb7465ef5c5591343fa5fb4aaa81b148918
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-04-04 14:18:01 +0800

    根据基类重写ItemSeedsColorBlack类

--- src/main/java/com/github/mrstop/stdemo/item/ItemSeedsColorBlack.java
+++ src/main/java/com/github/mrstop/stdemo/item/ItemSeedsColorBlack.java
@@ -1,7 +1,18 @@
 package com.github.mrstop.stdemo.item;
 
+import com.github.mrstop.stdemo.block.BlockLoader;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+
 public class ItemSeedsColorBlack extends ItemSeedsColor {
     public ItemSeedsColorBlack() {
-        this.setUnlocalizedName("seedsColorBlack");
+        super(BlockLoader.colorBlockBlack, "seedsColorBlack");
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iIconRegister) {
+        this.itemIcon = iIconRegister.registerIcon("seeds_color_black");
     }
 }
