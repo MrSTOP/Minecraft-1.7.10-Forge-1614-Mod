c742856d9fa03e19f883b6ad172602f4e2b449c4
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-04-05 21:15:26 +0800

    为原始花增加根据染料变异的效果

--- src/main/java/com/github/mrstop/stdemo/block/BlockColorFlowerOrigin.java
+++ src/main/java/com/github/mrstop/stdemo/block/BlockColorFlowerOrigin.java
@@ -3,11 +3,61 @@ package com.github.mrstop.stdemo.block;
 import com.github.mrstop.stdemo.STDemo;
 import com.github.mrstop.stdemo.item.ItemLoader;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class BlockColorFlowerOrigin extends BlockColorFlower {
     public BlockColorFlowerOrigin() {
         super("colorFlowerOriginBlock");
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, int x, int y, int z, EntityPlayer player, int side, float subX, float subY, float subZ) {
+//        player.addChatMessage(new ChatComponentText(player.getHeldItem().getItem().getClass().toString()));
+//        for (int i = 1000; i <= 1022; i++) {
+//            worldIn.playAuxSFX(2005, x, y, z, 0);
+//        }
+//        for (int i = 2000; i <= 2006; i++) {
+//            worldIn.playAuxSFX(2005, x, y, z, 0);
+//        }
+        float rate = 0.1F;
+        boolean cancelBlockPlace = false;
+        if (worldIn.isRemote) {
+            cancelBlockPlace = true;
+        } else {
+            for (int i: OreDictionary.getOreIDs(player.getHeldItem())) {
+                if (worldIn.rand.nextFloat() < rate) {
+                    cancelBlockPlace = true;
+                    if (i == OreDictionary.getOreID("dyeBlack")) {
+                        worldIn.setBlock(x, y, z, BlockLoader.blockColorFlowerBlack);
+                    } else if (i == OreDictionary.getOreID("dyeWhite")) {
+                        worldIn.setBlock(x, y, z, BlockLoader.blockColorFlowerWhite);
+                    } else if (i == OreDictionary.getOreID("dyeRed")) {
+                        worldIn.setBlock(x, y, z, BlockLoader.blockColorFlowerRed);
+                    } else if (i == OreDictionary.getOreID("dyeGreen")) {
+                        worldIn.setBlock(x, y, z, BlockLoader.blockColorFlowerGreen);
+                    } else if (i == OreDictionary.getOreID("dyeBlue")) {
+                        worldIn.setBlock(x, y, z, BlockLoader.blockColorFlowerBlue);
+                    } else if (i == OreDictionary.getOreID("dyeYellow")) {
+                        worldIn.setBlock(x, y, z, BlockLoader.blockColorFlowerYellow);
+                    } else if (i == OreDictionary.getOreID("dyeBrown")) {
+                        worldIn.setBlock(x, y, z, BlockLoader.blockColorFlowerBrown);
+                    } else {
+                        cancelBlockPlace = false;
+                    }
+                    if (cancelBlockPlace) {
+                        worldIn.playAuxSFX(2006, x, y, z, 0);
+                    }
+                } else {
+                    cancelBlockPlace = false;
+                }
+            }
+        }
+        return cancelBlockPlace;
     }
 
     @Override
