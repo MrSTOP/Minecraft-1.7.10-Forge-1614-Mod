01c5431f4f2c13e885c4e3cae7cf5eccac9afc72
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-04-10 21:55:00 +0800

    完善命令本地化和格式化

--- src/main/java/com/github/mrstop/stdemo/command/CommandPosition.java
+++ src/main/java/com/github/mrstop/stdemo/command/CommandPosition.java
@@ -5,7 +5,10 @@ import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentTranslation;
+
+import java.util.List;
 
 public class CommandPosition extends STDemoCommandBase {
     private static final String COMMAND_SUCCESS = "commands.position.success";
@@ -26,20 +29,34 @@ public class CommandPosition extends STDemoCommandBase {
 
     @Override
     public void processCommand(ICommandSender sender, String[] args) throws CommandException {
-        System.out.println("COMMAND");
-        System.out.println("++++++++++++++++");
+        System.out.print("COMMAND\n");
+        System.out.print("++++++++++ARGS++++++++++\n");
         for (String arg: args) {
             System.out.print(arg);
         }
-        System.out.println("++++++++++++++++");
+        System.out.print("\n++++++++++++++++++++++++\n");
 
         if (args.length > 1) {
             throw new WrongUsageException(COMMAND_USAGE);
         } else {
             EntityPlayerMP entityPlayerMP = args.length > 0 ? CommandBase.getPlayer(sender, args[0])
                     : CommandBase.getCommandSenderAsPlayer(sender);
-            sender.addChatMessage(new ChatComponentTranslation(COMMAND_SUCCESS, entityPlayerMP.getDisplayName(), entityPlayerMP.worldObj.provider.getDimensionName(), entityPlayerMP.posX, entityPlayerMP.posY, entityPlayerMP.posZ));
+            sender.addChatMessage(new ChatComponentTranslation(COMMAND_SUCCESS,
+                                                               entityPlayerMP.getDisplayName(),
+                                                               String.format("%.2f", entityPlayerMP.posX),
+                                                               String.format("%.2f", entityPlayerMP.posY),
+                                                               String.format("%.2f", entityPlayerMP.posZ),
+                                                               entityPlayerMP.worldObj.provider.getDimensionName()));
         }
+    }
+
+    @Override
+    public List addTabCompletionOptions(ICommandSender sender, String[] args) {
+        if (args.length == 1) {
+            String[] names = MinecraftServer.getServer().getAllUsernames();
+            return CommandBase.getListOfStringsMatchingLastWord(args, names);
+        }
+        return null;
     }
 
     @Override
